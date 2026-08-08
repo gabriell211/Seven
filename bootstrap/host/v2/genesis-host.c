ec2a28e9a1d934533ba949cd6ba6b097a8c31862
Gabriel Barcelos <102088315+gabriell211@users.noreply.github.com>
2026-08-08 12:06:31 -0300

    layer transition syscalls over audited genesis host

--- bootstrap/host/v2/genesis-host.c
+++ bootstrap/host/v2/genesis-host.c
@@ -1,5 +1,8 @@
 #include "genesis-host.part00.inc"
 #include "genesis-host.part01.inc"
+#define syscall genesis_base_syscall
 #include "genesis-host.part02.inc"
 #include "genesis-host.part03.inc"
+#undef syscall
+#include "genesis-host.transition.inc"
 #include "genesis-host.part04.inc"
